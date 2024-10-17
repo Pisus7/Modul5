abed5d713ea5ffc62f20c846ddf42fd4ba3fad88
piusrauch <pirauch@tsn.at>
2024-10-17 14:00:21 +0200

    Datenfeld erstellt Konstruktor erstellt methode erstellt

--- src/Angebot.java
+++ src/Angebot.java
@@ -1,10 +1,29 @@
 import java.time.LocalDate;
+import java.time.Month;
 
 
 public class Angebot {
     private LocalDate flugdatum;
     private double regulaererPreis;
     private String flugnummer;
+    private Rabattstrategie rabattstrategie;
 
+    public Angebot(LocalDate flugdatum, double regulaererPreis, String flugnummer) {
+        this.flugdatum = flugdatum;
+        this.regulaererPreis = regulaererPreis;
+        this.flugnummer = flugnummer;
+
+
+    }
+
+    public void rabattstrategieErkennen() {
+
+
+        if (flugdatum.getMonth()== Month.JANUARY || flugdatum.getMonth()== Month.APRIL || flugdatum.getMonth()== Month.OCTOBER) {
+
+
+        }
+
+    }
 
 }
